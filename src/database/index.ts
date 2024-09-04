2096568f18031c9f76794facbf2a585ca8cfe626
manasiTayade23 <mansitayade23@gmail.com>
2024-09-04 20:48:00 +0530

    code refactored

---
+++ src/database/index.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import { Coupon } from '../models/Coupen';
+
+export const AppDataSource = new DataSource({
+  type: ,
+  host: ,
+  port: ,
+  username: ,
+  password: ,
+  database: ,
+  synchronize: ,
+  logging: ,
+  entities: [],
+  migrations: [],
+  subscribers: [],
+});
