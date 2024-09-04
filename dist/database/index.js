467c4fcfbb0ea86f2b9fd7e3b4e380cc72de5fee
Yogini Tayade <105501691+manasiTayade23@users.noreply.github.com>
2024-09-04 20:49:35 +0530

    Update index.js

--- dist/database/index.js
+++ dist/database/index.js
@@ -1,18 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppDataSource = void 0;
-const typeorm_1 = require("typeorm");
-const Coupen_1 = require("../models/Coupen");
-exports.AppDataSource = new typeorm_1.DataSource({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'coupons',
-    synchronize: true,
-    logging: false,
-    entities: [Coupen_1.Coupon],
-    migrations: [],
-    subscribers: [],
+import { DataSource } from 'typeorm';
+import { Coupon } from '../models/Coupen';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'database_name',
+  synchronize: true,
+  logging: false,
+  entities: [Coupon],
+  migrations: [],
+  subscribers: [],
 });
