8c4a43f2393f5febb83ec4ea1eeceb07c67d3753
manasiTayade <mansitayade23@gmail.com>
2024-09-06 09:43:08 +0530

    removed node-modules

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,15 +2,15 @@ import { DataSource } from 'typeorm';
 import { Coupon } from '../models/Coupen';
 
 export const AppDataSource = new DataSource({
-  type: ,
-  host: ,
-  port: ,
-  username: ,
-  password: ,
-  database: ,
-  synchronize: ,
-  logging: ,
-  entities: [],
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'database_name',
+  synchronize: true,
+  logging: false,
+  entities: [Coupon],
   migrations: [],
   subscribers: [],
 });
